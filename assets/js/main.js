381db65261383079f6628f80418900a3b0270db3
Almuzaini <62063512+YAlmuzaini@users.noreply.github.com>
2021-03-28 15:12:40 +0300

    v4

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,9 +6,11 @@
     if ($(this).scrollTop() > 100) {
       $('.back-to-top').fadeIn('slow');
       $('#header').addClass('header-fixed');
+      $('#topbar').addClass('topbar-scrolled');
     } else {
       $('.back-to-top').fadeOut('slow');
       $('#header').removeClass('header-fixed');
+      $('#topbar').removeClass('topbar-scrolled');
     }
   });
 
